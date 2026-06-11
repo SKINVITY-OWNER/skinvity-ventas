ab7705c187a932d1afd5ba0e642586a6c9582bb5
SKINVITY-OWNER <blancasan@skinvity.com>
2026-06-11 15:50:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-const CACHE = 'skinvity-v6';
 
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
